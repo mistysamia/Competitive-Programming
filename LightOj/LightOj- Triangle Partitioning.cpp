48c706192013de98c0716484af7d05859dd3639c
Misty Samia <62679682+mistysamia@users.noreply.github.com>
2021-05-13 05:33:20 +0600

    Add files via upload

---
+++ LightOj/LightOj- Triangle Partitioning.cpp
@@ -0,0 +1,36 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define sf scanf
+#define pf printf
+#define pb push_back
+#define llu unsigned long long
+#define U unsigned int
+#define SIZE 100005
+#define pie 3.14159265358979323
+#define minuss 1e-6
+#define pair pair<ll,ll>
+#define fast_cin ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define BIG 1000000000
+#define N 10000
+/*
+std::cout << std::fixed;
+std::cout << std::setprecision(1);
+std::cout << VALUE NAME<<endl;
+*/
+ll tra[100006],tra1[100006];
+
+
+int main()
+{
+    fast_cin;
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+
+    }
+}
+
