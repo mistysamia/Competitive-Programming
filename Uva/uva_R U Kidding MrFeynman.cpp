d620f3d8d89ed916e923d636c605d4ce07fe7eb9
Misty Samia <62679682+mistysamia@users.noreply.github.com>
2021-05-19 04:54:40 +0600

    Add files via upload

---
+++ Uva/uva_R U Kidding MrFeynman.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define pb push_back
+#define MP make_pair
+#define llu unsigned long long
+#define U unsigned int
+#define ff first
+#define ss second
+#define all(v) (v).begin(), (v).end()
+#define b(n) cout<<"bug "<<n<<endl;
+#define SIZE 100005
+#define pie 3.14159265358979323
+#define minuss 1e-6
+#define pii pair<ll,ll>
+#define fast_cin ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define BIG 1000000000
+#define INF 1000000000000005
+#define N 10000
+
+ll mod=1e9+7;
+
+//GCD=__gcd(n,m);  lcm=(n*m)/GCD;
+//cout<<fixed<<setprecision(1)<<VALUE NAME<<endl;
+
+ll tra[100006],tra1[100006];
+
+
+int main()
+{
+    fast_cin;
+    ll t,cas=1;
+   // cin>>t;
+    while(1)
+    {
+        //map<ll,ll>mp;
+       // map<ll,ll>::iterator it;
+        string s,s1;
+       // deque<ll>deq;
+        double lock,sum,co,ans,a,b,c=0,d=0,dx;
+        double n,m,k,mx=-BIG,mn=BIG;
+        ll fir=0,sec=0,x=0,y=0;
+        cin>>n;
+        if(n==0.0)
+            break;
+
+        a=floor(cbrt(n));
+        //cout<<a<<endl;
+        dx=(n-(a*a*a))/(3.0*a*a);
+        sum=a+dx;
+
+        cout<<fixed<<setprecision(4)<<sum<<endl;
+    }
+}
+
